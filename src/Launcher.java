22b15ce86e1e10e31f73052a717e49979285df22
Somayeh <somaye.hosinnia33@gmail.com>
2023-10-04 19:43:00 +0200

    rename

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,11 +4,11 @@ public class Launcher {
 	public static void main(String[] args) {
 		int size = 3;
 		Board board= new TicTacToeBoard(size);
-		Player[] players= new TicTacToePlayer[2];
+		Player[] players= new Player[2];
 		players[0]= new TicTacToePlayer('A', "Ali");
 		players[1]= new TicTacToePlayer('B', "Sara");
-		TicTacToeEngine model= new TicTacToeEngine(board, players);
-		TicTacToeView  view= new TicTacToeView (size);
+		Engine model= new TicTacToeEngine(board, players);
+		View  view= new TicTacToeView (size);
 		TicTacToeController controller= new TicTacToeController(model, view, size);
 
 	}
